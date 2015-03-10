0dc3a7eb4cb37146d325577a5471469896a26c37
annahpryor <annahpryor@yahoo.com>
2015-03-10 00:06:37 -0700

    first commit

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,26 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y){
+    x <<-y
+    m <<- NULL
+   }
 
-}
 
 
-## Write a short comment describing this function
+get <- function() x
+
+
+## Return a matrix that is the inverse of x
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        invx <- solve(x)
+        invx
+}
+
+getcacheSolve <- function()invx
+list(set = set, get = get,
+     cacheSolve = cacheSolve,
+     getcacheSolve = getcacheSolve)
 }
